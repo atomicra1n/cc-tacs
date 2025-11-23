96dfc7421a112e3a7c14712f012ef81b2b9bff50
atomicra1n <megami@tenseimegami.com>
2025-11-23 01:24:55 +0100

    Update main.lua

--- minter/main.lua
+++ minter/main.lua
@@ -1,1 +1,21 @@
 print("main of minter")
+
+-- CLI Loop to keep the program running
+while true do
+    term.setTextColor(colors.yellow)
+    write("Minter> ")
+    term.setTextColor(colors.white)
+    
+    local input = read()
+    
+    if input == "exit" then
+        print("Exiting minter...")
+        break
+    elseif input == "help" then
+        print("Available commands:")
+        print(" mint <user> - Create a new card")
+        print(" help        - Show this menu")
+    elseif input ~= "" then
+        print("Unknown command: " .. input)
+    end
+end
